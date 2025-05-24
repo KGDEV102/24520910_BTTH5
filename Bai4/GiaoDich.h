681a42e2990b4dbf97e49bcfb264beafe9765012
KGDEV102 <anhkietphan2402.tg@gmail.com>
2025-05-24 21:43:31 +0700

    Create GiaoDich.h

---
+++ Bai4/GiaoDich.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class GiaoDich
+{
+protected:
+	string MGD;
+	
+	double DonGia;
+	double DienTich;
+public:
+	string NgayGD;
+	void Nhap();
+	void Xuat();
+};
+
